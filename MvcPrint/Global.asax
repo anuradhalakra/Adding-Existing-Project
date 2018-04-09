f45a825a72b955b153a97164d733f51bb0a9b9d3
Anuradha Lakra <anuradha.lakra@indiaebix.com>
2018-04-09 02:42:12 -0400

    First commit

---
+++ MvcPrint/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcPrint.MvcApplication" Language="C#" %>
---
+++ MvcPrint/Models/HomeModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+using System.Collections.Generic;
+namespace MvcPrint.Models
+{
+    public class HomeModel 
+    {
+        public List<SelectListItem> LocalPrinters { get; set; }
+        public List<SelectListItem> NetworkPrinters { get; set; }
+    }
+}
